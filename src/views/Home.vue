7a65156457b4688a6b1728593caaef969a75f2a7
Carlos <cl72506@gmail.com>
2020-12-21 10:15:12 -0400

    axios instance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+/* import HelloWorld from "@/components/HelloWorld.vue"; */
+import service from '../services/service.js';
 export default {
   name: "Home",
   components: {
-    HelloWorld
+/*     HelloWorld */
+  },
+  data(){ return{
+    productos:[]
+  }},
+  methods:{
+    async getProducts(){
+      await service.myApi.get('product').then((response) => {
+          const data = response.data;
+          this.productos = data.data;
+          console.log(this.productos);
+          
+        })
+        .catch((e) => {
+          console.log(e);
+       
+          
+        });
+    }
+  },
+  created(){
+    
+    this.getProducts();
   }
 };
 </script>
